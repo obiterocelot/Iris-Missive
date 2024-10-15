a73098ee776ca7daac7fca0607830e68abe0bda0
Sarah Kelly <sarahblairkelly@gmail.com>
2024-10-15 20:15:35 +1300

    deleting old file paths

--- src/routes/posts.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getAllPosts } from '../controllers/postController';
-
-const postRouter = express.Router();
-
-// GET request to retrieve all posts
-postRouter.get("/posts", getAllPosts);
-
-export default postRouter;
--- src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getUser } from '../controllers/userController';
-
-const userRouter = express.Router();
-
-// GET request to retrieve user from id
-userRouter.get("/users/:id", getUser);
-
-export default userRouter
